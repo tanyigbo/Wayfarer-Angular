0402861a01625a2cdfb5a8efa4f2cafcc3115cd5
wmavery <wanda.avery@att.net>
2023-05-24 22:07:06 -0500

    added trending travel verbiage for topic on landing page and updated app name on navbar to Wayfarer

--- src/app/landing-page/topic/topic.component.ts
+++ src/app/landing-page/topic/topic.component.ts
@@ -1,15 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { TOPICS } from '../topic/topics'
 
 @Component({
   selector: 'app-topic',
   templateUrl: './topic.component.html',
   styleUrls: ['./topic.component.css']
 })
-export class TopicComponent {
+export class TopicComponent implements OnInit {
+  // declare and initalize public members
+  topicCnt = new Array(3);
+  topics: string[] = [];
+  titles: string[] = [];;
+ 
+  // method will obtain and set trending topic titles
+  getTitles() : any {
+    TOPICS.map(topic => {
+      this.titles.push(topic.title);
+    });
+  }
+  // method will obtain and set trending topics
+  getTopics() : any {
+    TOPICS.map(topic => {
+      this.topics.push(topic.topic);
+    });
+  }
 
-  longText: string = `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos 
-  veritatis eius, ipsa molestias nesciunt ipsam reiciendis necessitatibus 
-  soluta cumque excepturi temporibus ab, 
-  quisquam cupiditate alias a, odit possimus nostrum beatae.`;
-  topics = new Array(3);
+  ngOnInit(): void {
+    // call method to display trending travel info
+    this.getTitles();
+    this.getTopics();
+  }
+
 }
