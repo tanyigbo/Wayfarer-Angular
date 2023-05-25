63af88402fdf2cde2b4f253749622e5c75cb4761
wmavery <wanda.avery@att.net>
2023-05-25 12:35:54 -0500

    added post data to post.ts and iconic images to /assets/images for each city

--- src/app/cities/cities.component.ts
+++ src/app/cities/cities.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { cities } from '../../assets/data/cities';
+import { posts } from '../../assets/data/posts';
 
 @Component({
   selector: 'app-cities',
   templateUrl: './cities.component.html',
   styleUrls: ['./cities.component.css']
 })
-export class CitiesComponent {
+export class CitiesComponent implements OnInit {
   cityData = cities;
+
+
+  ngOnInit(): void {
+  }
 }
