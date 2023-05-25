ecda4dc3e8b87a3c6f3ddf92e9f8d7be2521d8eb
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-05-25 02:51:10 -0500

    refactored topics dynamic allocation

--- src/app/landing-page/topic/topic.component.ts
+++ src/app/landing-page/topic/topic.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { TOPICS } from '../topic/topics'
 
@@ -7,29 +7,6 @@ import { TOPICS } from '../topic/topics'
   templateUrl: './topic.component.html',
   styleUrls: ['./topic.component.css']
 })
-export class TopicComponent implements OnInit {
-  // declare and initalize public members
-  topicCnt = new Array(3);
-  topics: string[] = [];
-  titles: string[] = [];;
- 
-  // method will obtain and set trending topic titles
-  getTitles() : any {
-    TOPICS.map(topic => {
-      this.titles.push(topic.title);
-    });
-  }
-  // method will obtain and set trending topics
-  getTopics() : any {
-    TOPICS.map(topic => {
-      this.topics.push(topic.topic);
-    });
-  }
-
-  ngOnInit(): void {
-    // call method to display trending travel info
-    this.getTitles();
-    this.getTopics();
-  }
-
+export class TopicComponent {
+  topics2 = TOPICS;
 }
