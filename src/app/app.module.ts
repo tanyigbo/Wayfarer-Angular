63a177c483b340ad84cfad0b8d96f91073157daa
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-05-25 03:19:55 -0500

    refactored material ui modules into seperate module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MaterialUiModule } from './material-ui/material-ui.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-
 import { LandingPageModule } from './landing-page/landing-page.module';
 import { CitiesComponent } from './cities/cities.component';
-
-import {MatCardModule} from '@angular/material/card';
 import { SearchComponent } from './search/search.component';
 
 @NgModule({
@@ -26,8 +20,7 @@ import { SearchComponent } from './search/search.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule, MatButtonModule, MatMenuModule, MatIconModule, 
-    LandingPageModule, MatCardModule,
+    LandingPageModule, MaterialUiModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
