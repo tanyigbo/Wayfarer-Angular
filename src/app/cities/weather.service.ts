aa9ec7961b27ad21fa4a7f83e8131898d2961518
builde7b0b <ggives3@gmail.com>
2023-05-26 11:43:00 -0700

    implement class

--- src/app/cities/weather.service.ts
+++ src/app/cities/weather.service.ts
@@ -1,3 +1,23 @@
 import { Injectable } from '@angular/core';
 import axios from 'axios'
 
+@Injectable({
+    providedIn: 'root'
+
+})
+
+export class WeatherService {
+    private apiKey = '';
+
+    async getCurrentWeather(cityName: string): Promise<any> {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${this.apiKey}`
+        try { 
+            const response = await axios.get(url);
+            return response.data;
+
+        } catch (error) {
+            console.error('Error fetching weather data:', error);
+            throw error
+        }
+    }
+}
