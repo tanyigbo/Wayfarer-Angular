574867e424d30f1b1a899c9a4262383dab3d0a94
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-05-26 21:38:37 -0500

    rolled back chagnes to cities component

--- src/app/cities/cities.component.ts
+++ src/app/cities/cities.component.ts
@@ -8,25 +8,11 @@ import { WeatherService } from './weather.service';
   styleUrls: ['./cities.component.css']
 })
 export class CitiesComponent implements OnInit {
-  cityData: { id: number; cityName: string; imgAddress: string; posts: any[]; weather: any }[] = [];
-
-  constructor(private weatherService: WeatherService) {}
+  cityData: any;
+  constructor(private weatherService: WeatherService) { }
 
   ngOnInit(): void {
-    this.fetchWeatherData();
+    this.cityData = cities;
   }
 
-  async fetchWeatherData(): Promise<void> {
-    for (const city of cities) {
-      const weatherData = await this.weatherService.getCurrentWeather(city.city);
-      const cityData = {
-        id: city.id,
-        cityName: city.city,
-        imgAddress: city.imgAddres, // Corrected property name
-        posts: [], // Replace with your posts data or remove this line if not needed
-        weather: weatherData
-      };
-      this.cityData.push(cityData);
-    }
-  }
 }
