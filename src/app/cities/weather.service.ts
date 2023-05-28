a093c3233d7533a70ccf232bfd71baaf0ff5037e
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-05-28 14:26:46 -0500

    formatting changes

--- src/app/cities/weather.service.ts
+++ src/app/cities/weather.service.ts
@@ -10,7 +10,7 @@ export class WeatherService {
     private apiKey = '62d308ad62832561583c79af0b3b556c';
 
     async getCurrentWeather(cityName: string): Promise<any> {
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${this.apiKey}`
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${this.apiKey}&&units=imperial`
         try { 
             const response = await axios.get(url);
             return response.data;
