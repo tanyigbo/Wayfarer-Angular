fe79842df98ea0d4dd329a2db61fb75a8bdf16d6
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-05-29 20:46:04 -0500

    removed unnecessary code

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { ActiveUrlService } from '../active-url.service';
 
 @Component({
@@ -7,14 +6,12 @@ import { ActiveUrlService } from '../active-url.service';
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css'],
 })
+
 export class NavComponent implements OnInit {
 
   protected searchBarActive: boolean = false;
 
-  constructor(protected router: Router,
-    private urlService: ActiveUrlService) {
-
-  }
+  constructor(private urlService: ActiveUrlService) { }
 
   ngOnInit(): void {
     this.urlService.getActiveUrl().subscribe((url) => {
