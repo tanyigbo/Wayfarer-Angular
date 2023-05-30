8084bc0e8a80b8c454f5ed5a0ab22a023e902bbf
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-05-29 20:45:09 -0500

    refactored search bar to only appear in city-display

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ActiveUrlService } from '../active-url.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css'],
 })
-export class NavComponent {
-  constructor(protected router: Router) { }
+export class NavComponent implements OnInit {
+
+  protected searchBarActive: boolean = false;
+
+  constructor(protected router: Router,
+    private urlService: ActiveUrlService) {
+
+  }
+
+  ngOnInit(): void {
+    this.urlService.getActiveUrl().subscribe((url) => {
+      this.searchBarActive = url ? true : false
+    })
+  }
+
 }
