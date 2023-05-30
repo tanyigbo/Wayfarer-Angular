511d1fc1dfc5f2d57411571a3c821cce6e5130b8
Tobe Anyigbo <98617728+tanyigbo@users.noreply.github.com>
2023-05-29 21:02:19 -0500

    search bar clears when switching cities and shows city name

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SearchService } from '../search.service';
+import { ActiveUrlService } from '../active-url.service';
+import { cities } from 'src/assets/data/cities';
 
 
 @Component({
@@ -11,8 +13,12 @@ import { SearchService } from '../search.service';
 export class SearchComponent implements OnInit {
 
   searchInput: string = '';
+  cityId: number = 0;
+  cityData: any = cities;
 
-  constructor(private searchService: SearchService) { }
+  constructor(
+    private searchService: SearchService,
+    private urlService: ActiveUrlService) { }
 
   // get the search input and send to service
   getSearchedPost(text: string) {
@@ -21,7 +27,10 @@ export class SearchComponent implements OnInit {
   }
 
   ngOnInit(): void {
-      
+    this.urlService.getActiveUrl().subscribe((url) => {
+      this.cityId = url ? parseInt(url) : 0;
+      this.searchInput = '';
+    })
   }
 
 }
